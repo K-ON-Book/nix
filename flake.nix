f012b5279e93b46be36db864d1e24168cfadbaf9
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-03-27 23:42:39 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "My journey of learning nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            mdbook
+          ];
+          shellHook = ''
+            cd tutorial/
+            export PS1="\e[0;32m(nix)\$ \e[m" 
+          '';
+        };
+      };
+    };
+}
